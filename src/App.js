de448420860ae4169f25bca7959b1cda034cd8ce
meriem23 <esra.msallem@gmail.com>
2022-07-02 12:33:41 +0100

    api exp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react"
 import './App.css';
 import AddTodo from './components/AddTodo';
 import FilterTodo from './components/FilterTodo';
 import ListTodo from './components/ListTodo';
 
 function App() {
+  /*API */
+  const [info, setInfo] = useState(null)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setInfo(data));
+  }, [])
+  console.log(info);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +20,15 @@ function App() {
         <FilterTodo />
         <ListTodo />
       </header>
+      <div>
+        {info.map((el) =>
+          <div style={{ border: "2px red solid" }}>
+            <h1>{el.name}</h1>
+            <h1>{el.username}</h1>
+            <h1>{el.email}</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
